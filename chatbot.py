ed632acfbe5fe8e30f0eea96157b7804595fe830
Rahul <pentarahul03@gmail.com>
2026-08-28 13:28:47 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -34,7 +34,7 @@ for message in st.session_state.chat_history:
 
 # LLM initiate
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="llama-3.3-70b-versatile",
     temperature=0.0,
     api_key=api_key,
 )
